64cd27444ebbfdf8ac4719794aec3d8407962ec7
DelusionaL <monimoysaha315@gmail.com>
2021-04-27 18:35:41 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+curl -L https://raw.githubusercontent.com/dELUSION-MS/dotfiles/main/.vimrc -o $HOME/.vimrc
+curl -L https://raw.githubusercontent.com/dELUSION-MS/dotfiles/main/.tmux.conf -o $HOME/.tmux.conf
