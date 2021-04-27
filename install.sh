293fe55a82c6012dbf8d622240369a5a70aa8456
DelusionaL <delusional0@pm.me>
2021-04-27 18:38:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-curl -L https://raw.githubusercontent.com/dELUSION-MS/dotfiles/main/.vimrc -o $HOME/.vimrc
-curl -L https://raw.githubusercontent.com/dELUSION-MS/dotfiles/main/.tmux.conf -o $HOME/.tmux.conf
+curl -sL https://raw.githubusercontent.com/dELUSION-MS/dotfiles/main/.vimrc -o $HOME/.vimrc
+curl -sL https://raw.githubusercontent.com/dELUSION-MS/dotfiles/main/.tmux.conf -o $HOME/.tmux.conf
